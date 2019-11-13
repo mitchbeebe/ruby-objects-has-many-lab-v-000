ddb987988b8dce58a083855a6c4279bd3de71edc
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 16:13:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,16 @@
 class Song
   
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist, :artist_name
   
   @@all = []
   
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
 end
